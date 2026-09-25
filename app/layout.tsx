6f4fbd4643a47b7063624ed1b8e7c6d3758c6058
Frezzard2 <kukucska.zsombor@gmail.com>
2026-09-25 09:12:01 +0100

    Add Tricolour tokens, direction system and flag rail
    The flag rail renders in both directions, overriding the prototype's
    --cF:none, because it is the element the creators specifically asked to
    keep everywhere.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Antonio:wght@400;600;700&family=Public+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Saira+Stencil+One&family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
